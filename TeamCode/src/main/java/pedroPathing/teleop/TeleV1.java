3dea3b42ffe2eac6fce0b4e13079fcbf165c806e
Ashray Shah <geektraindev@gmail.com>
2025-04-20 14:54:00 -0500

    Bro what did I commit :skull:

--- TeamCode/src/main/java/pedroPathing/teleop/TeleV1.java
+++ TeamCode/src/main/java/pedroPathing/teleop/TeleV1.java
@@ -53,7 +53,6 @@ public class TeleV1 extends OpMode {
     @Override
     public void start() {
 //        follower.startTeleopDrive();
-<<<<<<< HEAD
     }
 
     void updateDriveVelocities() {
@@ -90,58 +89,13 @@ public class TeleV1 extends OpMode {
         leftBack.setPower(backLeftPower);
         rightFront.setPower(frontRightPower);
         rightBack.setPower(backRightPower);
-=======
->>>>>>> origin/master
     }
 
     /** This is the main loop of the opmode and runs continuously after play **/
     @Override
     public void loop() {
 
-<<<<<<< HEAD
 
-=======
-        /* Update Pedro to move the robot based on:
-        - Forward/Backward Movement: -gamepad1.left_stick_y
-        - Left/Right Movement: -gamepad1.left_stick_x
-        - Turn Left/Right Movement: -gamepad1.right_stick_x
-        - Robot-Centric Mode: false
-        */
-        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-        double x = gamepad1.left_stick_x;
-        double rx = gamepad1.right_stick_x;
-
-        // This button choice was made so that it is hard to hit on accident,
-        // it can be freely changed based on preference.
-        // The equivalent button is start on Xbox-style controllers.
-        if (gamepad1.options) {
-            imu.resetYaw();
-        }
-
-        follower.update();
-//        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        double botHeading = follower.getPose().getHeading();
-        // Rotate the movement direction counter to the bot's rotation
-        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-        rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-        double frontLeftPower = (rotY + rotX + rx) / denominator;
-        double backLeftPower = (rotY - rotX + rx) / denominator;
-        double frontRightPower = (rotY - rotX - rx) / denominator;
-        double backRightPower = (rotY + rotX - rx) / denominator;
-
-        leftFront.setPower(frontLeftPower);
-        leftBack.setPower(backLeftPower);
-        rightFront.setPower(frontRightPower);
-        rightBack.setPower(backRightPower);
-
->>>>>>> origin/master
 //        follower.setTeleOpMovementVectors(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x, false);
 
         /* Telemetry Outputs of our Follower */
